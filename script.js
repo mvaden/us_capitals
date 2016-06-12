296f9cca6994080433770664392ebf07f9810be7
Matt <mvaden08@gmail.com>
2016-06-12 18:34:44 -0400

    Created a few variables.

---
+++ script.js
@@ -0,0 +1,11 @@
+var question;
+var territories;
+var territory_captials;
+
+question = "What is the state captial of";
+territories = [ "U.S. Virgin Islands", "Puerto Rico", "Northern Marina Islands", "Guam" ];
+territory_captials = [ "Charlotte Amalie", "San Juan", "Saipan", "Hagåtña" ];
+
+var states = [ "Alaska", "Alabama", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Deleware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisana", "Maine", "Maryland", "Massachusetts", "Michigan" "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jeresy", "New Mexico", "New York", "North Carolina" "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvaina", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "West Virgnia", "Wisonsin", "Wyoming" ];
+
+var state_capitals = [ "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", "Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Illiois", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "St. Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismark", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madisn", "Cheyenne" ]
